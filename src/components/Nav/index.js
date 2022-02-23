2957fd4afc6b9b9f9c937612cc4645fc15b0c840
Austin Gilmore <austingilmora@gmail.com>
2022-02-23 13:40:43 -0500

    conditionally render contact

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,7 +7,9 @@ function Nav(props) {
     const {
         categories = [],
         setCurrentCategory,
-        currentCategory
+        currentCategory,
+        contactSelected,
+        setContactSelected
     } = props
     
 
@@ -25,20 +27,21 @@ function Nav(props) {
             <nav>
                 <ul className='flex-row'>
                     <li className='mx-2'>
-                        <a data-testid="about" href='#about'>
+                        <a data-testid="about" href='#about' onClick={() => setContactSelected(false)}>
                             About Me
                         </a>
                     </li>
-                    <li>
-                        <span>Contact</span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                     {
                         categories.map((category) => (
                             <li className={`mx-1 ${
-                                currentCategory.name === category.name && 'navActive'
+                                currentCategory.name === category.name && !contactSelected && 'navActive'
                                 }`} key={category.name}>
                                 <span 
                                     onClick={() => {
+                                        setContactSelected(false)
                                         setCurrentCategory(category)
                                     }}
                                 >
